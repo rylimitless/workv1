43657ac1e3e3ce5e92f804a0dfbf3f4310603436
ry <ry.limitless@gmail.com>
2025-03-28 15:06:55 -0500

    adding payloads

--- main.go
+++ main.go
@@ -4,34 +4,61 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"maps"
 	"net/http"
+	"sync"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gorilla/websocket"
 )
 
-var messageHandler mqtt.MessageHandler = func(c mqtt.Client, m mqtt.Message) {
-	message := m.Payload()
-	data := make(map[string]interface{})
-	err := json.Unmarshal(message, &data)
-	// fmt.Println(string(m.Payload()))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for keys := range maps.Values(data) {
-		fmt.Println(keys)
-	}
-
+type Sockets struct {
+	mu          sync.Mutex
+	connections map[*websocket.Conn]bool
 }
 
-var connectionHandler mqtt.OnConnectHandler = func(c mqtt.Client) {
-	fmt.Println("connected")
+var (
+	sockets        Sockets
+	messageHandler mqtt.MessageHandler = func(c mqtt.Client, m mqtt.Message) {
+		message := m.Payload()
+		data := make(map[string]interface{})
+		err := json.Unmarshal(message, &data)
+		// fmt.Println(string(m.Payload()))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		sockets.mu.Lock()
+		defer sockets.mu.Unlock()
+
+		for connection, _ := range sockets.connections {
+			connection.WriteJSON(data)
+		}
+
+	}
+
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			// Allow all origins during development
+			return true
+		},
+	}
+	connectionHandler mqtt.OnConnectHandler = func(c mqtt.Client) {
+		fmt.Println("connected")
+	}
+)
+
+func (s *Sockets) Subscribe(client mqtt.Client) {
+	token := client.Subscribe("hello/world", 1, nil)
+	token.Wait()
 }
 
 func main() {
+
+	sockets = Sockets{
+		connections: make(map[*websocket.Conn]bool),
+	}
 
 	// print("Hello World")
 	ops := mqtt.NewClientOptions()
@@ -44,16 +71,25 @@ func main() {
 		panic(token.Error())
 	}
 
-	token := client.Subscribe("hello/world", 1, nil)
-	token.Wait()
+	sockets.Subscribe(client)
 
 	// client.
 
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+	r.Get("/sock", func(w http.ResponseWriter, r *http.Request) {
+		c, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Print("upgrade:", err)
+			return
+		}
+		defer c.Close()
+
+		sockets.connections[c] = true
+
+		for {
+		}
 	})
 
 	err := http.ListenAndServe(":3000", r)
