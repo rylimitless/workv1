db41db697780694893ae43a3a711fe6ad4faf26e
ry <ry.limitless@gmail.com>
2025-03-28 02:20:52 -0500

    add websockets

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module web
 go 1.24.1
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,54 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"maps"
 	"net/http"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+var messageHandler mqtt.MessageHandler = func(c mqtt.Client, m mqtt.Message) {
+	message := m.Payload()
+	data := make(map[string]interface{})
+	err := json.Unmarshal(message, &data)
+	// fmt.Println(string(m.Payload()))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for keys := range maps.Values(data) {
+		fmt.Println(keys)
+	}
+
+}
+
+var connectionHandler mqtt.OnConnectHandler = func(c mqtt.Client) {
+	fmt.Println("connected")
+}
+
 func main() {
+
 	// print("Hello World")
+	ops := mqtt.NewClientOptions()
+	ops.AddBroker("tcp://broker.emqx.io:1883")
+	ops.OnConnect = connectionHandler
+	ops.SetDefaultPublishHandler(messageHandler)
+
+	client := mqtt.NewClient(ops)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	token := client.Subscribe("hello/world", 1, nil)
+	token.Wait()
+
+	// client.
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
@@ -21,5 +60,7 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	client.Disconnect(250)
 
 }
